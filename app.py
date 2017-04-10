d8a6b48e6e22d0488a8a78254cff06b5eae837b2
Brooks Kindle <brookskindle+git@gmail.com>
2017-04-09 20:28:19 -0700

    Add command to freeze site into static files

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
+
+@app.cli.command()
+def freeze():
+    """Freeze the site into a set of static files"""
+    freezer.freeze()
+
 
 @app.route("/")
 def index():
