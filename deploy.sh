5bd63cfa58ecafcccaf3a5a896a23275a18a5f03
Brooks Kindle <brookskindle+git@gmail.com>
2017-04-09 20:53:31 -0700

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-    python manage.py freeze
+    flask freeze
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
